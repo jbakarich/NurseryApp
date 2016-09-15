893a9fe2c0b98787bdf9565df3132496b309d983
jbakarich <jacob.bakarich@gmail.com>
2016-09-15 17:20:29 -0600

    Nursery App
    Initial upload from WIP menu app

---
+++ app/src/main/java/com/example/jacob/messingwithmenus/SecondFragment.java
@@ -0,0 +1,30 @@
+package com.example.jacob.messingwithmenus;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+/**
+ * Created by jacob on 9/15/2016.
+ */
+public class SecondFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+        myView = inflater.inflate(R.layout.second_layout, container, false);
+
+
+
+
+        return myView;
+    }
+}
