7d557ab9c7d96fee38026414ec0e268552614d0f
Cyrille Gindreau <Cyrille@Cyrilles-MacBook-Pro.local>
2016-09-29 11:40:37 -0600

    updated gradle, created Attendence Fragment

---
+++ app/src/main/java/team3/nmsu/NurseryApp/AttendenceFragment.java
@@ -0,0 +1,26 @@
+package team3.nmsu.NurseryApp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import team3.nmsu.NurseryApp.test.R;
+
+/**
+ * Created by cyrille on 9/29/2016.
+ */
+public class AttendenceFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+        return myView;
+    }
+}
