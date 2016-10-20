8f0701163add2dc01d49c58d28b060ce308ef6bd
Cyrille Gindreau <Cyrille@Cyrilles-MacBook-Pro.local>
2016-10-20 17:14:48 -0600

    asdfghjkl

---
+++ app/src/main/java/team3/nmsu/NurseryApp/HomeFragment.java
@@ -0,0 +1,31 @@
+package team3.nmsu.NurseryApp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import team3.nmsu.NurseryApp.test.R;
+
+/**
+ * Created by jacob on 9/15/2016.
+ */
+public class HomeFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+       // myView = inflater.inflate(R.layout.HomeFragment, container, false);
+
+
+
+
+        return myView;
+    }
+}
