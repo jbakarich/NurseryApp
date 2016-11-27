260d1ba6cc60af62ae79ef3b22927a29f0fa8c1a
Cyrille Gindreau <cyrillegin@gmail.com>
2016-11-27 14:41:41 -0700

    admin profile now updates user info correctly

--- app/src/main/java/com/MispronouncedDevelopment/Homeroom/Admin_ProfileFragment.java
+++ app/src/main/java/com/MispronouncedDevelopment/Homeroom/Admin_ProfileFragment.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -82,13 +83,25 @@ public class Admin_ProfileFragment extends Fragment {
     void UpdateProfile(JSONObject response) {
         Log.d(TAG, "got a response: " + response.toString());
 
+        TextView firstName = (TextView) getView().findViewById(R.id.admin_profileName);
+//        TextView userName = (TextView) getView().findViewById(R.id.admin_);
+//        TextView lastName = (TextView) getView().findViewById(R.id.admin_l);
+//        TextView childName = (TextView) getView().findViewById(R.id.admin_);
+        TextView phone = (TextView) getView().findViewById(R.id.admin_profilePhone);
+        TextView address1 = (TextView) getView().findViewById(R.id.admin_profileAddress1);
+        TextView address2 = (TextView) getView().findViewById(R.id.admin_profileAddress2);
+        TextView email = (TextView) getView().findViewById(R.id.admin_profileEmail);
 
 
-//        try {
-//
-//
-//        } catch (JSONException e) {
-//            Log.d(TAG, "err in response:" + e.toString());
-//        }
+        try {
+            firstName.setText(response.getString("username"));
+            phone.setText(response.getString("phone"));
+            address1.setText(response.getString("address1"));
+            address2.setText(response.getString("address2"));
+            email.setText(response.getString("email"));
+
+        } catch (JSONException e) {
+            Log.d(TAG, "err in response:" + e.toString());
+        }
     }
 }
